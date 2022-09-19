16b140ec200de99e861dff653f905a83dd3e1c1c
mazzy <mazzy8@mail.ru>
2022-09-19 18:05:35 +0500

    first tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 from .pages.main_page import MainPage
-from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,3 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
